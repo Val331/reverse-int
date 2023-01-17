68fcd146d93dca242b7c2ddaba9e5068c17ad448
Valera Mamrovsky <valeramamrovsky@gmail.com>
2023-01-17 12:30:19 +0300

    feat: answer reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    n = String(Math.abs(n));
+    while (n[n.length - 1] === '0') {
+        n = n.slice(0, n.length - 1);
+    }
+    for (let i of n) {result = `${i}${result}`;}
+    return result;
 }
